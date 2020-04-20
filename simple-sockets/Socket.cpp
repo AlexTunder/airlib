6e67b48e1e0ce05bc781771713868f1ec46dcfc5
alexThunders <rakutin2005@mail.ru>
2020-04-20 16:50:03 +0000

    simple-socket v.0.0.4. Simple listener class & ___SockPTR__. Updated listener function example

--- simple-sockets/Socket.cpp
+++ simple-sockets/Socket.cpp
@@ -16,6 +16,19 @@ int connectLS(int __fd, const sockaddr *__addr, socklen_t __len){
 int listenLS(int __fd, int __n){
     return listen(__fd, __n);
 }
+int sideTo(int *array, int start, int end){
+    array[start] = 0;
+    for(int i = start; i < end; i++){
+        array[i] = array[i+1];
+    }
+    end = 0;
+}
+int sideTo(int **array, int start, int end){
+    for(int i = start; i < end; i++){
+        array[i] = array[i+1];
+    }
+    array[end] = nullptr;
+}
 
 /** Basic socket interface **/
 int Socket::send(std::string data){
@@ -52,6 +65,13 @@ int Socket::setBitrade(int bitrade){
         this->bitrade = bitrade;
     return this->bitrade;
 }
+int Socket::kill(int conn){
+    delete (this->connection[conn]);
+    sideTo(this->connection, conn, cc);
+}
+// int Socket::stop(int conn){
+
+// }
 
 /** socket Server interface **/
 
@@ -79,7 +99,7 @@ is null or not resolved.", nullptr, port, -3));
 }
 void *SocketServer::listen(void *arg){
     throwenSocketServerStruct *me = (throwenSocketServerStruct*)(arg);
-    while(me->socketServer->state){
+    while(1){
         me->socketServer->connection[me->socketServer->cc] = new int(0);
         me->socketServer->threadStorage[me->socketServer->cc] = new pthread_t();
         if (listenLS(me->socketServer->fd, 3) < 0)
@@ -91,13 +111,14 @@ void *SocketServer::listen(void *arg){
             SOCKET_REPORTING("New user connected\n");
         #endif
         me->connectionID = me->socketServer->cc;
-        pthread_create(me->socketServer->threadStorage[me->socketServer->cc], &me->socketServer->attr, me->listener, arg);
+        pthread_create(me->socketServer->threadStorage[me->socketServer->cc], &me->socketServer->attr, me->listener, new throwNetAttr(me, me->connectionID));
         pthread_detach(*me->socketServer->threadStorage[me->socketServer->cc]);
         me->socketServer->cc++;
     }
 }
 void SocketServer::start(void *(*listener)(void *arg), int maxClients){
     this->arg = {this, listener};
+	this->arg.sockPtr = new ___SockPTR___uniQED__(___SockPTR___uniQED__(this, 1));
     this->state = 1;
     pthread_create(&mainThread, &attr, this->listen, &arg);
     pthread_detach(mainThread);
@@ -119,4 +140,103 @@ int SocketClient::connect(std::string addr, int port, int protocol){
  or already in use. Failing in connection stage.", nullptr, port, -2));
     connection[0] = &fd;
     cc++;
+}
+
+/*** NetworkListener interface **/
+
+void NetworkListener::setStopCom(char com[3]){
+    for(int i = 0; i < 3; i++)
+        this->command[0][i] = com[i];
+}
+void NetworkListener::setStreanCom(char com[3]){
+    for(int i = 0; i < 3; i++)
+        this->command[1][i] = com[i];
+}
+void NetworkListener::setFileRequestCom(char com[3]){
+    for(int i = 0; i < 3; i++)
+        this->command[2][i] = com[i];
+}
+
+void NetworkListener::SetListener(int type, void (*listener)(void *attr)){
+    if(type == DEFAULT_LISTENER)
+        this->listeners.defaultListener = listener;
+    else if(type == FILE_REQUEST_LISTENER)
+        this->listeners.fileReaquestListener = listener;
+    else if(type == STOP_LISTENER)
+        this->listeners.stopListener = listener;
+    else if(type == STREAM_LISTENER)
+        this->listeners.streamListener = listener;
+    else throw(SocketException((char*)"localhost/programm", (char*)"unknowen listener type", (char*)std::to_string(type).c_str(), 0x000, -9));
+}
+
+void NetworkListener::send(void *arg, char *data){
+	netServerAttribures *attr = (netServerAttribures*)(arg);
+	___SockPTR___uniQED__ *sockptr = getSockPointerFromAttr(attr);
+    sockptr->send(data, sockptr->connn);
+}
+char *NetworkListener::read(void *arg){
+	netServerAttribures *attr = (netServerAttribures*)(arg);
+	___SockPTR___uniQED__ *sockptr = getSockPointerFromAttr(attr);
+    return (char*)(sockptr->read(sockptr->connn)).c_str();
+}
+void NetworkListener::__Main__(void *arg){
+	netServerAttribures *attr = (netServerAttribures*)(arg);
+	___SockPTR___uniQED__ *sockptr = getSockPointerFromAttr(attr);
+	this->listeners.defaultListener(arg);
+}
+
+std::string ___SockPTR___uniQED__::read(){
+    readLS(*conn[*cc-1], this->buffer, *this->bitrade);
+    if(buffer == NULL) throw(SocketException((char*)"Current serverd", (char*)"Failed to read incoming data, \
+becouse it's is NULL", nullptr, 71, -1));
+    return std::string((char*)(buffer));
+}
+std::string ___SockPTR___uniQED__::read(int conn){
+    char *buf = this->buffer;
+    if(readLS(*this->conn[conn], this->buffer, *this->bitrade)<0) printf("readLS is 0");
+    this->buffer = buf;
+    if(buffer == nullptr) throw(SocketException((char*)"Current serverd", (char*)"Failed to read incoming data, \
+becouse it's is NULL", nullptr, 71, -1));
+    std::string str = this->buffer;
+    return str;
+}
+int ___SockPTR___uniQED__::send(std::string data){
+    return sendLS(*conn[*cc-1], data.c_str(), *this->bitrade, 0);
+}
+int ___SockPTR___uniQED__::send(std::string data, int conn){
+    return sendLS(*this->conn[conn], data.c_str(), *this->bitrade, 0);
+}
+char *___SockPTR___uniQED__::setBuffer(char *buffer){
+    this->buffer = buffer;
+    return buffer;
+}
+int ___SockPTR___uniQED__::setBitrade(int bitrade){
+    *this->bitrade = bitrade;
+    return bitrade;
+}
+___SockPTR___uniQED__::___SockPTR___uniQED__(void *ptr, bool isServer){
+    if(isServer){
+        SocketServer *server = (SocketServer*)(ptr);
+        this->fd = &server->fd;
+        this->conn = server->connection;
+        this->buffer = server->buffer;
+        this->bitrade = &server->bitrade;
+        this->cc = &server->cc;
+    }else{
+        SocketClient *client = (SocketClient*)(ptr);
+        this->fd = &client->fd;
+        this->conn = client->connection;
+        this->buffer = client->buffer;
+        this->bitrade = &client->bitrade;
+        this->cc = &client->cc;
+    }
+}
+
+___SockPTR___uniQED__* getSockPointerFromAttr(SocketServer::throwenSocketServerStruct *attr){
+	return attr->sockPtr;
+}
+
+throwNetAttr::throwNetAttr(netServerAttribures *attr, int conn){
+    this->ptsd = attr;
+    this->ptsd->sockPtr->connn = this->connID = conn;
 }
--- simple-sockets/Socket.hpp
+++ simple-sockets/Socket.hpp
@@ -13,12 +13,16 @@
 #include <net/if.h>
 #include <iostream>
 #include <pthread.h>
+#define DEFAULT_LISTENER        0
+#define STOP_LISTENER           1
+#define FILE_REQUEST_LISTENER   2
+#define STREAM_LISTENER         3
 
 class Socket{
     public:
         /** Comment this later **/
         Socket();
-        void *buffer;
+        char *buffer;
         int bitrade;
         struct sockaddr_in address; 
         int fd, **connection, opt, cc;
@@ -37,6 +41,27 @@ class Socket{
         char *setBuffer(char *buffer);
 };
 
+class ___SockPTR___uniQED__{
+    public:
+        ___SockPTR___uniQED__(void *client, bool isServer);
+        int *fd;
+        int **conn;
+        int *cc;
+        int *bitrade;
+        char *buffer;
+        /** Communicate with connection **/
+        std::string read();
+        std::string read(int conn);
+        int send(std::string data);
+        int send(std::string data, int conn);
+        /** Connection managment; **/
+        int kill(int conn);
+        int close(int conn);
+        int setBitrade(int bitrade);
+        char *setBuffer(char *buffer);
+        int connn;
+};
+
 class SocketException{
     public:
         SocketException(char *address, char *description, char *additional, int port, int code);
@@ -44,27 +69,53 @@ class SocketException{
         int port, codeOfError;
 };
 
+class NetworkListener;
+
 class SocketServer : public Socket{
     public:
         struct throwenSocketServerStruct{
-            SocketServer *socketServer;
+            SocketServer *socketServer = nullptr;
             void *(*listener)(void *arg);
             int connectionID;
+            ___SockPTR___uniQED__ *sockPtr = nullptr;
         };
         /*** I should to use this structure, becouse it's have dependes ***/
         int bind(std::string addr, int port, int protocol = SOCK_STREAM);
         void start(void *(*listener)(void *arg), int maxClients);
+        void start(NetworkListener listener, int maxClients);
         struct throwenSocketServerStruct arg;
         static void *listen(void *arg);
         pthread_t **threadStorage;
         int port; std::string me;
         pthread_t mainThread;
         pthread_attr_t attr;
-        bool state = true;
         SocketServer();
+        int state = 0;
 };
 
-
+class NetworkListener{
+    private:
+        char command[3][3];
+        struct listenersStruct{
+            void (*stopListener)(void *arg);
+            void (*streamListener)(void *arg);
+            void (*defaultListener)(void *arg);
+            void (*fileReaquestListener)(void *arg);
+            SocketServer::throwenSocketServerStruct *arg;
+        }listeners;
+        void *(*mainListener)(void *parg);
+        void __Main__(void *arg);
+        Socket *targer;
+    public:
+        char*read(void *arg);
+        void setStopCom(char com[3]);
+        void setStreanCom(char com[3]);
+        void send(void *arg,char *data);
+        void setFileRequestCom(char com[3]);
+        char*read(void (*descriptor)(char *));
+        void send(char *data, void (*descriptor)(char *));
+        void SetListener(int type, void (*listener)(void *parg));
+};
 
 class SocketClient : public Socket{
     public:
@@ -91,6 +142,15 @@ class Decoder : public Coder{
 
 void anonimusEncode(char*, char*);
 void anonimusDecode(char*, char*);
+
+___SockPTR___uniQED__* getSockPointerFromAttr(SocketServer::throwenSocketServerStruct *attr);
+
+class throwNetAttr{
+    public:
+        SocketServer::throwenSocketServerStruct *ptsd;
+        int connID;
+        throwNetAttr(SocketServer::throwenSocketServerStruct *netData, int connID); 
+};
 
 #define netServerAttribures SocketServer::throwenSocketServerStruct
 #include "Socket.cpp"
